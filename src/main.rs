1b9913222cd736497b930d8a02c3b4ad40e2da86
Patrique Ouimet <patrique.ouimet@gmail.com>
2020-11-11 21:01:32 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket::Request;
+use rocket::response::content::Json;
+use rocket_contrib::templates::Template;
+use serde::Serialize;
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Oh no! We couldn't find the requested path '{}'", req.uri())
+}
+
+#[get("/hello")]
+fn hello() -> Template {
+    #[derive(Serialize)]
+    struct Context { name: String }
+    let context = Context { name: String::from("Patrique Ouimet") };
+    Template::render("index", &context)
+}
+
+#[get("/hello")]
+fn api_hello() -> Json<&'static str> {
+  Json("{\"status\": \"success\", \"message\": \"Hello API!\"}")
+}
+
+fn main() {
+    rocket::ignite()
+        .register(catchers![not_found])
+        .mount("/", routes![hello])
+        .mount("/api", routes![api_hello])
+        .attach(Template::fairing())
+        .launch();
+}
